29c64645f34bbfbca0e87b8f641a1fe2e715d5ac
jrheard <jrheard@cs.stanford.edu>
2020-07-11 22:21:10 -0700

    standardize on 2b writeup terminology

--- src/two.rs
+++ src/two.rs
@@ -1,19 +1,19 @@
 use std::fs;
 
 pub fn two_a() -> i32 {
-    let mut tape = load_tape();
+    let mut memory = load_program("src/inputs/2.txt");
 
     // Before running the program, replace position 1 with the value 12
     // and replace position 2 with the value 2.
-    tape[1] = 12;
-    tape[2] = 2;
+    memory[1] = 12;
+    memory[2] = 2;
 
     // What value is left at position 0 after the program halts?
-    process_tape(tape)[0]
+    run_program(memory)[0]
 }
 
-fn load_tape() -> Vec<i32> {
-    let contents = fs::read_to_string("src/inputs/2.txt").unwrap();
+fn load_program(filename: &str) -> Vec<i32> {
+    let contents = fs::read_to_string(filename).unwrap();
 
     contents
         .trim()
@@ -22,9 +22,9 @@ fn load_tape() -> Vec<i32> {
         .collect()
 }
 
-fn process_tape(tape: Vec<i32>) -> Vec<i32> {
+fn run_program(memory: Vec<i32>) -> Vec<i32> {
     let mut instruction_pointer = 0;
-    let mut result = tape.clone();
+    let mut result = memory.clone();
 
     loop {
         let opcode = result[instruction_pointer];
@@ -32,13 +32,13 @@ fn process_tape(tape: Vec<i32>) -> Vec<i32> {
         if opcode == 99 {
             break;
         } else {
-            let index_1 = result[instruction_pointer + 1] as usize;
-            let index_2 = result[instruction_pointer + 2] as usize;
+            let address_1 = result[instruction_pointer + 1] as usize;
+            let address_2 = result[instruction_pointer + 2] as usize;
             let destination = result[instruction_pointer + 3] as usize;
 
             match opcode {
-                1 => add(&mut result, index_1, index_2, destination),
-                2 => mul(&mut result, index_1, index_2, destination),
+                1 => add(&mut result, address_1, address_2, destination),
+                2 => mul(&mut result, address_1, address_2, destination),
                 _ => panic!("unknown opcode {}", opcode),
             }
         }
@@ -48,12 +48,12 @@ fn process_tape(tape: Vec<i32>) -> Vec<i32> {
     result
 }
 
-fn add(tape: &mut Vec<i32>, index_1: usize, index_2: usize, destination: usize) {
-    tape[destination] = tape[index_1] + tape[index_2];
+fn add(memory: &mut Vec<i32>, address_1: usize, address_2: usize, destination: usize) {
+    memory[destination] = memory[address_1] + memory[address_2];
 }
 
-fn mul(tape: &mut Vec<i32>, index_1: usize, index_2: usize, destination: usize) {
-    tape[destination] = tape[index_1] * tape[index_2];
+fn mul(memory: &mut Vec<i32>, address_1: usize, address_2: usize, destination: usize) {
+    memory[destination] = memory[address_1] * memory[address_2];
 }
 
 #[cfg(test)]
@@ -61,23 +61,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_process_tape() {
-        assert_eq!(process_tape(vec![1, 0, 0, 0, 99]), vec![2, 0, 0, 0, 99]);
-        assert_eq!(process_tape(vec![2, 3, 0, 3, 99]), vec![2, 3, 0, 6, 99]);
+    fn test_run_program() {
+        assert_eq!(run_program(vec![1, 0, 0, 0, 99]), vec![2, 0, 0, 0, 99]);
+        assert_eq!(run_program(vec![2, 3, 0, 3, 99]), vec![2, 3, 0, 6, 99]);
         assert_eq!(
-            process_tape(vec![2, 4, 4, 5, 99, 0]),
+            run_program(vec![2, 4, 4, 5, 99, 0]),
             vec![2, 4, 4, 5, 99, 9801]
         );
         assert_eq!(
-            process_tape(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]),
+            run_program(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]),
             vec![30, 1, 1, 4, 2, 5, 6, 0, 99]
         );
     }
 
     #[test]
-    fn test_load_tape() {
+    fn test_load_program() {
         assert_eq!(
-            load_tape(),
+            load_program("src/inputs/2.txt"),
             vec![
                 1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 13, 1, 19, 1, 5, 19, 23, 2, 10,
                 23, 27, 1, 27, 5, 31, 2, 9, 31, 35, 1, 35, 5, 39, 2, 6, 39, 43, 1, 43, 5, 47, 2,
