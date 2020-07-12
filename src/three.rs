b4b263db1e7a6348b7cff81c6e880c8d740b938e
jrheard <jrheard@cs.stanford.edu>
2020-07-12 11:27:57 -0700

    add tests for examples given in 3a writeup

--- src/three.rs
+++ src/three.rs
@@ -5,7 +5,11 @@ use std::io::BufReader;
 
 pub fn three_a() -> i32 {
     let (wire_1, wire_2) = load_wires();
+    closest_intersection(wire_1, wire_2)
+}
 
+/// Returns the manhattan distance of the two wires' closest intersection to 0,0.
+fn closest_intersection(wire_1: String, wire_2: String) -> i32 {
     let wire_1_positions = parse_wire(wire_1);
     let wire_2_positions = parse_wire(wire_2);
 
@@ -111,6 +115,24 @@ mod tests {
         }
 
         assert_eq!(parse_wire(String::from("R8,U5,L5,D3")), set);
+    }
+
+    #[test]
+    fn test_closest_intersection() {
+        assert_eq!(
+            closest_intersection(
+                String::from("R75,D30,R83,U83,L12,D49,R71,U7,L72"),
+                String::from("U62,R66,U55,R34,D71,R55,D58,R83")
+            ),
+            159
+        );
+        assert_eq!(
+            closest_intersection(
+                String::from("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
+                String::from("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
+            ),
+            135
+        );
     }
 
     #[test]
