d1c44b4974347e2d5c9a7d40a1aefcf9805a6e99
jrheard <jrheard@cs.stanford.edu>
2020-07-26 20:05:52 -0700

    refactor 13a

--- src/thirteen.rs
+++ src/thirteen.rs
@@ -2,7 +2,51 @@ use crate::computer;
 use crate::computer::{Computer, HaltReason};
 use std::collections::HashMap;
 
-type Game = HashMap<(usize, usize), Tile>;
+struct Game {
+    state: HashMap<(i64, i64), Tile>,
+    computer: Computer,
+}
+
+impl Game {
+    pub fn new() -> Game {
+        let memory = computer::load_program("src/inputs/13.txt");
+
+        Game {
+            state: HashMap::new(),
+            computer: Computer::new(memory, vec![]),
+        }
+    }
+
+    fn update_state(&mut self) {
+        loop {
+            let halt_reason = self.computer.run(HaltReason::Output);
+            if halt_reason == HaltReason::Exit {
+                break;
+            }
+
+            self.computer.run(HaltReason::Output);
+            self.computer.run(HaltReason::Output);
+
+            // "The software draws tiles to the screen with output instructions: every
+            // three output instructions specify the x position (distance from the left), y
+            // position (distance from the top), and tile id."
+            let tile_id = self.computer.pop_output().unwrap();
+            let y = self.computer.pop_output().unwrap();
+            let x = self.computer.pop_output().unwrap();
+
+            let tile = match tile_id {
+                0 => Tile::Empty,
+                1 => Tile::Wall,
+                2 => Tile::Block,
+                3 => Tile::Paddle,
+                4 => Tile::Ball,
+                _ => panic!("unexpected tile {}", tile_id),
+            };
+
+            self.state.insert((x, y), tile);
+        }
+    }
+}
 
 #[derive(PartialEq)]
 enum Tile {
@@ -20,40 +64,12 @@ enum Tile {
 
 /// "Start the game. How many block tiles are on the screen when the game exits?"
 pub fn thirteen_a() -> usize {
-    let memory = computer::load_program("src/inputs/13.txt");
-    let mut computer = Computer::new(memory, vec![]);
-
-    let mut game = HashMap::new();
-
-    loop {
-        let halt_reason = computer.run(HaltReason::Output);
-        if halt_reason == HaltReason::Exit {
-            break;
-        }
-
-        computer.run(HaltReason::Output);
-        computer.run(HaltReason::Output);
-
-        // "The software draws tiles to the screen with output instructions: every
-        // three output instructions specify the x position (distance from the left), y
-        // position (distance from the top), and tile id."
-        let tile_id = computer.pop_output().unwrap();
-        let y = computer.pop_output().unwrap();
-        let x = computer.pop_output().unwrap();
-
-        let tile = match tile_id {
-            0 => Tile::Empty,
-            1 => Tile::Wall,
-            2 => Tile::Block,
-            3 => Tile::Paddle,
-            4 => Tile::Ball,
-            _ => panic!("unexpected tile {}", tile_id),
-        };
-
-        game.insert((x, y), tile);
-    }
-
-    game.values().filter(|&tile| tile == &Tile::Block).count()
+    let mut game = Game::new();
+    game.update_state();
+    game.state
+        .values()
+        .filter(|&tile| tile == &Tile::Block)
+        .count()
 }
 
 #[cfg(test)]
@@ -62,6 +78,6 @@ mod tests {
 
     #[test]
     fn test_solutions() {
-        assert_eq!(thirteen_a(), 0);
+        assert_eq!(thirteen_a(), 284);
     }
 }
