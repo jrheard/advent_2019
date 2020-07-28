288d24d248fe40623d588d7f8decd823cc9bacdd
jrheard <jrheard@cs.stanford.edu>
2020-07-27 17:37:10 -0700

    tweak 14a, i don't think i need a Recipe struct after all

--- src/fourteen.rs
+++ src/fourteen.rs
@@ -1,10 +1,12 @@
 use once_cell::sync::Lazy;
 use regex::Regex;
+use std::collections::HashMap;
+use std::fs;
 
 static OUTER_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"(.*) => (.*)").unwrap());
 static COMPONENT_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"([0-9]*) ([A-Z]*)").unwrap());
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Eq, Debug, Hash)]
 struct RecipeComponent {
     chemical: String,
     quantity: u32,
@@ -21,26 +23,26 @@ impl RecipeComponent {
     }
 }
 
-#[derive(PartialEq, Debug)]
-struct Recipe {
-    inputs: Vec<RecipeComponent>,
-    output: RecipeComponent,
-}
+fn parse_recipe(recipe: &str) -> (Vec<RecipeComponent>, RecipeComponent) {
+    let captures = OUTER_RE.captures(recipe).unwrap();
+    let inputs = captures[1].split(", ").map(RecipeComponent::new).collect();
 
-impl Recipe {
-    pub fn new(recipe: &str) -> Recipe {
-        let captures = OUTER_RE.captures(recipe).unwrap();
-        let inputs = captures[1].split(", ").map(RecipeComponent::new).collect();
-
-        Recipe {
-            inputs,
-            output: RecipeComponent::new(&captures[2]),
-        }
-    }
+    (inputs, RecipeComponent::new(&captures[2]))
 }
 
 pub fn fourteen_a() -> u32 {
+    let _recipes = load_recipes();
+
     5
+}
+
+fn load_recipes() -> HashMap<RecipeComponent, Vec<RecipeComponent>> {
+    let contents = fs::read_to_string("src/inputs/14.txt").unwrap();
+    contents
+        .lines()
+        .map(parse_recipe)
+        .map(|(inputs, output)| (output, inputs))
+        .collect()
 }
 
 #[cfg(test)]
@@ -50,8 +52,8 @@ mod tests {
     #[test]
     fn test_parse_recipe() {
         assert_eq!(
-            Recipe::new("7 LCSV, 1 LKPNB, 36 CMNH, 1 JZXPH, 20 DGJPN, 3 WDWB, 69 DXJKC, 3 WHJKH, 18 XSGP, 22 CGZL, 2 BNVB, 57 PNSD => 1 FUEL"),
-            Recipe { inputs: vec![RecipeComponent { chemical: "LCSV".to_string(), quantity: 7 }, RecipeComponent { chemical: "LKPNB".to_string(), quantity: 1 }, RecipeComponent { chemical: "CMNH".to_string(), quantity: 36 }, RecipeComponent { chemical: "JZXPH".to_string(), quantity: 1 }, RecipeComponent { chemical: "DGJPN".to_string(), quantity: 20 }, RecipeComponent { chemical: "WDWB".to_string(), quantity: 3 }, RecipeComponent { chemical: "DXJKC".to_string(), quantity: 69 }, RecipeComponent { chemical: "WHJKH".to_string(), quantity: 3 }, RecipeComponent { chemical: "XSGP".to_string(), quantity: 18 }, RecipeComponent { chemical: "CGZL".to_string(), quantity: 22 }, RecipeComponent { chemical: "BNVB".to_string(), quantity: 2 }, RecipeComponent { chemical: "PNSD".to_string(), quantity: 57 }], output: RecipeComponent { chemical: "FUEL".to_string(), quantity: 1 } }
+            parse_recipe("7 LCSV, 1 LKPNB, 36 CMNH, 1 JZXPH, 20 DGJPN, 3 WDWB, 69 DXJKC, 3 WHJKH, 18 XSGP, 22 CGZL, 2 BNVB, 57 PNSD => 1 FUEL"),
+            (vec![RecipeComponent { chemical: "LCSV".to_string(), quantity: 7 }, RecipeComponent { chemical: "LKPNB".to_string(), quantity: 1 }, RecipeComponent { chemical: "CMNH".to_string(), quantity: 36 }, RecipeComponent { chemical: "JZXPH".to_string(), quantity: 1 }, RecipeComponent { chemical: "DGJPN".to_string(), quantity: 20 }, RecipeComponent { chemical: "WDWB".to_string(), quantity: 3 }, RecipeComponent { chemical: "DXJKC".to_string(), quantity: 69 }, RecipeComponent { chemical: "WHJKH".to_string(), quantity: 3 }, RecipeComponent { chemical: "XSGP".to_string(), quantity: 18 }, RecipeComponent { chemical: "CGZL".to_string(), quantity: 22 }, RecipeComponent { chemical: "BNVB".to_string(), quantity: 2 }, RecipeComponent { chemical: "PNSD".to_string(), quantity: 57 }], RecipeComponent { chemical: "FUEL".to_string(), quantity: 1 })
         );
     }
 }
