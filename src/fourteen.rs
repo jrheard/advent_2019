8744668a8940ba5ec5d634e03ecf8832290bd817
jrheard <jrheard@cs.stanford.edu>
2020-07-29 09:15:14 -0700

    WIP on 14a, failing sample_4

--- src/fourteen.rs
+++ src/fourteen.rs
@@ -1,4 +1,3 @@
-use itertools::Itertools;
 use once_cell::sync::Lazy;
 use regex::Regex;
 use std::collections::{HashMap, VecDeque};
@@ -169,7 +168,6 @@ fn collapse_bulk_buy_nodes(
 fn cost_for_one_fuel(recipes: &HashMap<String, Recipe>) -> u32 {
     let mut root = Node::new("FUEL".to_string(), 1);
     naively_fill_tree(&mut root, recipes);
-    dbg!(&root);
 
     let bulk_buy_chemicals: Vec<String> = recipes
         .values()
@@ -185,127 +183,6 @@ fn cost_for_one_fuel(recipes: &HashMap<String, Recipe>) -> u32 {
     while collapse_bulk_buy_nodes(&mut root, &recipes, &bulk_buy_chemicals) {}
 
     total_quantity_of_chemical_in_tree(&root, "ORE")
-}
-
-// TODO see if i can remove this step once i get 14a passing
-/// TODO oh my god rename and document
-fn perform_simple_reductions(
-    components: &[RecipeComponent],
-    recipes: &HashMap<String, Recipe>,
-) -> Vec<RecipeComponent> {
-    components
-        .iter()
-        .flat_map(|component| {
-            if component.chemical == "ORE" {
-                // Ore's our base material, there isn't a recipe for it.
-                return vec![component.clone()];
-            }
-
-            let recipe = &recipes[&component.chemical];
-
-            if recipe.output.quantity == 1 {
-                if recipe.inputs.len() == 1 && recipe.inputs[0].chemical == "ORE" {
-                    // We've bottomed out for this particular component for now!
-                    vec![component.clone()]
-                } else {
-                    // Substitute `recipe` out for its constituent parts.
-                    perform_simple_reductions(&recipe.inputs, recipes)
-                        .iter()
-                        .map(|reduction_component| RecipeComponent {
-                            chemical: reduction_component.chemical.clone(),
-                            quantity: reduction_component.quantity * component.quantity,
-                        })
-                        .collect()
-                }
-            } else {
-                vec![component.clone()]
-            }
-        })
-        .collect()
-}
-
-fn merge_components(components: &[RecipeComponent]) -> Vec<RecipeComponent> {
-    let grouped_components = components
-        .iter()
-        .map(|component| (&component.chemical, component))
-        .into_group_map();
-
-    grouped_components
-        .iter()
-        .map(|(chemical, component_group)| RecipeComponent {
-            chemical: (*chemical).clone(),
-            quantity: component_group
-                .iter()
-                .map(|component| component.quantity)
-                .sum(),
-        })
-        .collect()
-}
-
-/// TODO oh my god rename and document
-fn component_costs(
-    components: &[RecipeComponent],
-    recipes: &HashMap<String, Recipe>,
-) -> Vec<RecipeComponent> {
-    components
-        .iter()
-        .flat_map(|component| {
-            if component.chemical == "ORE" {
-                return vec![component.clone()];
-            }
-
-            let recipe = &recipes[&component.chemical];
-
-            let desired_output_quantity = component.quantity;
-            let required_num_reactions =
-                (desired_output_quantity as f32 / recipe.output.quantity as f32).ceil();
-
-            recipe
-                .inputs
-                .iter()
-                .map(move |input_component| RecipeComponent {
-                    chemical: input_component.chemical.clone(),
-                    quantity: input_component.quantity * required_num_reactions as u32,
-                })
-                .collect()
-        })
-        .collect()
-
-    // TODO i think it might be a good idea to record a history of simplified things
-    // i can't tell if that's something that would make sense to do here, i.e. to have this
-    // return  a 2-tuple of (current_return_value, something_else)
-    // or if it's something that should happen in the loop, between step 4 and step 1
-    // figure it out tomorrow!!!!
-}
-
-fn cost_for_one_fuel_old(recipes: &HashMap<String, Recipe>) -> u32 {
-    let mut bulk_buys: HashMap<String, Vec<u32>> = HashMap::new();
-    let mut components = recipes["FUEL"].inputs.clone();
-    //println!("1: {:?}", components);
-
-    while components.len() > 1 {
-        components = perform_simple_reductions(&components, &recipes);
-        //println!("2: {:?}", components);
-        components = merge_components(&components);
-        //println!("3: {:?}", components);
-
-        for component in components.clone() {
-            bulk_buys
-                .entry(component.chemical)
-                .or_insert_with(Vec::new)
-                .push(component.quantity);
-        }
-
-        components = component_costs(&components, &recipes);
-        //println!("4: {:?}", components);
-        components = merge_components(&components);
-        //println!("5: {:?}", components);
-    }
-
-    dbg!(bulk_buys);
-
-    println!("6: {:?}", components);
-    components[0].quantity
 }
 
 pub fn fourteen_a() -> u32 {
@@ -327,72 +204,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_required_chemicals_for() {
-        let recipes = load_recipes("src/inputs/14_sample_1.txt");
-
-        assert_eq!(
-            perform_simple_reductions(
-                &[RecipeComponent {
-                    chemical: "FUEL".to_string(),
-                    quantity: 1
-                }],
-                &recipes
-            ),
-            vec![
-                RecipeComponent {
-                    chemical: "A".to_string(),
-                    quantity: 7
-                },
-                RecipeComponent {
-                    chemical: "A".to_string(),
-                    quantity: 7
-                },
-                RecipeComponent {
-                    chemical: "A".to_string(),
-                    quantity: 7
-                },
-                RecipeComponent {
-                    chemical: "A".to_string(),
-                    quantity: 7
-                },
-                RecipeComponent {
-                    chemical: "B".to_string(),
-                    quantity: 1
-                }
-            ]
-        );
-    }
-
-    #[test]
-    fn test_merge_components() {
-        let recipes = load_recipes("src/inputs/14_sample_1.txt");
-        let required = perform_simple_reductions(
-            &[RecipeComponent {
-                chemical: "FUEL".to_string(),
-                quantity: 1,
-            }],
-            &recipes,
-        );
-
-        let mut merged = merge_components(&required);
-        merged.sort_by_key(|component| component.chemical.to_string());
-
-        assert_eq!(
-            merged,
-            vec![
-                RecipeComponent {
-                    chemical: "A".to_string(),
-                    quantity: 28
-                },
-                RecipeComponent {
-                    chemical: "B".to_string(),
-                    quantity: 1
-                },
-            ]
-        );
-    }
-
-    #[test]
     fn test_parse_recipe() {
         assert_eq!(
             Recipe::new("7 LCSV, 1 LKPNB, 36 CMNH, 1 JZXPH, 20 DGJPN, 3 WDWB, 69 DXJKC, 3 WHJKH, 18 XSGP, 22 CGZL, 2 BNVB, 57 PNSD => 1 FUEL"),
@@ -402,14 +213,17 @@ mod tests {
 
     #[test]
     fn test_cost_for_one_fuel() {
-        //let recipes = load_recipes("src/inputs/14_sample_1.txt");
-        //assert_eq!(cost_for_one_fuel(&recipes), 31);
-
-        //let recipes = load_recipes("src/inputs/14_sample_3.txt");
-        //assert_eq!(cost_for_one_fuel(&recipes), 165);
+        let recipes = load_recipes("src/inputs/14_sample_1.txt");
+        assert_eq!(cost_for_one_fuel(&recipes), 31);
 
         let recipes = load_recipes("src/inputs/14_sample_2.txt");
         assert_eq!(cost_for_one_fuel(&recipes), 13312);
+
+        let recipes = load_recipes("src/inputs/14_sample_3.txt");
+        assert_eq!(cost_for_one_fuel(&recipes), 165);
+
+        let recipes = load_recipes("src/inputs/14_sample_4.txt");
+        assert_eq!(cost_for_one_fuel(&recipes), 180697);
     }
 
     #[test]
