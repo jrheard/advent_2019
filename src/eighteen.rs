16f3b10d3c675845b5f356002cb0faae966ad291
jrheard <jrheard@cs.stanford.edu>
2020-08-07 09:40:31 -0700

    WIP

--- src/eighteen.rs
+++ src/eighteen.rs
@@ -3,6 +3,9 @@ use std::fs;
 
 type Position = (usize, usize);
 
+/// A map of {key -> (distance_to_key_from_starting_position, doors_needed, keys_picked_up_on_the_way)}.
+type KeyDistanceMap = HashMap<Key, (u32, Bitfield, Bitfield)>;
+
 #[derive(Eq, PartialEq, Hash, Copy, Clone)]
 struct Key(u32);
 
@@ -90,7 +93,7 @@ impl Vault {
     }
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 struct Bitfield(u32);
 
 impl Bitfield {
@@ -114,6 +117,7 @@ fn one_position_ahead(direction: &Direction, position: &Position) -> Position {
     }
 }
 
+#[derive(Debug)]
 struct BfsNode {
     position: Position,
     distance: u32,
@@ -121,11 +125,8 @@ struct BfsNode {
     keys_picked_up: Bitfield,
 }
 
-/// Returns a map of {key -> (distance_to_key_from_starting_position, doors_needed, keys_picked_up_on_the_way)}.
-fn populate_key_distances_and_doors(
-    starting_position: Position,
-    vault: &Vault,
-) -> HashMap<Key, (u32, Bitfield, Bitfield)> {
+/// Returns a KeyDistanceMap of `vault` as seen from `starting_position`.
+fn populate_key_distances_and_doors(starting_position: Position, vault: &Vault) -> KeyDistanceMap {
     let self_key = match vault.get(starting_position.0, starting_position.1) {
         Space::Key(character) => character,
         _ => unreachable!(),
@@ -205,24 +206,26 @@ struct SearchNode {
 
 /// Returns the smallest distance that is necessary to travel while acquiring all of the keys in `keys_to_find`.
 fn find_shortest_path(
-    starting_key: Key,
     keys_to_find: Bitfield,
-    key_distances: &HashMap<Key, HashMap<Key, (u32, Bitfield, Bitfield)>>,
+    key_distances_per_vault: &[HashMap<Key, KeyDistanceMap>],
 ) -> u32 {
     let mut shortest_path = u32::MAX;
     let mut queue = VecDeque::new();
     let mut smallest_distance_for_path = HashMap::new();
 
     // Seed the queue.
-    for (&other_key, (distance, doors_needed, keys_along_the_way)) in &key_distances[&starting_key]
-    {
-        if doors_needed.0 == 0 {
-            queue.push_back(SearchNode {
-                distance: *distance,
-                key: other_key,
-                keys_acquired: Bitfield(keys_along_the_way.0 | other_key.0),
-                keys_left: Bitfield(keys_to_find.0 - other_key.0 - keys_along_the_way.0),
-            });
+    for key_distances in key_distances_per_vault {
+        for (&other_key, (distance, doors_needed, keys_along_the_way)) in
+            &key_distances[&STARTING_KEY]
+        {
+            if doors_needed.0 == 0 {
+                queue.push_back(SearchNode {
+                    distance: *distance,
+                    key: other_key,
+                    keys_acquired: Bitfield(keys_along_the_way.0 | other_key.0),
+                    keys_left: Bitfield(keys_to_find.0 - other_key.0 - keys_along_the_way.0),
+                });
+            }
         }
     }
 
@@ -250,23 +253,28 @@ fn find_shortest_path(
             smallest_distance_for_path.insert((keys_acquired, key), distance);
         }
 
-        for (&other_key, (distance_to_other_key, doors_needed, keys_along_the_way)) in
-            &key_distances[&key]
-        {
-            if distance + distance_to_other_key >= shortest_path {
-                continue;
-            }
-
-            if keys_left.0 & other_key.0 == other_key.0 && keys_acquired.contains_all(*doors_needed)
+        for key_distances in key_distances_per_vault {
+            for (&other_key, (distance_to_other_key, doors_needed, keys_along_the_way)) in
+                &key_distances[&key]
             {
-                queue.push_back(SearchNode {
-                    distance: distance + distance_to_other_key,
-                    key: other_key,
-                    keys_acquired: Bitfield(keys_acquired.0 | keys_along_the_way.0 | other_key.0),
-                    keys_left: Bitfield(
-                        keys_left.0 - (keys_left.0 & keys_along_the_way.0) - other_key.0,
-                    ),
-                });
+                if distance + distance_to_other_key >= shortest_path {
+                    continue;
+                }
+
+                if keys_left.0 & other_key.0 == other_key.0
+                    && keys_acquired.contains_all(*doors_needed)
+                {
+                    queue.push_back(SearchNode {
+                        distance: distance + distance_to_other_key,
+                        key: other_key,
+                        keys_acquired: Bitfield(
+                            keys_acquired.0 | keys_along_the_way.0 | other_key.0,
+                        ),
+                        keys_left: Bitfield(
+                            keys_left.0 - (keys_left.0 & keys_along_the_way.0) - other_key.0,
+                        ),
+                    });
+                }
             }
         }
     }
@@ -274,9 +282,7 @@ fn find_shortest_path(
     shortest_path
 }
 
-fn key_distance_maps_for_each_key_in_vault(
-    vault: &Vault,
-) -> HashMap<Key, HashMap<Key, (u32, Bitfield, Bitfield)>> {
+fn key_distance_maps_for_each_key_in_vault(vault: &Vault) -> HashMap<Key, KeyDistanceMap> {
     let mut key_distance_maps = HashMap::new();
     for (&key, &position) in &vault.keys {
         key_distance_maps.insert(
@@ -305,10 +311,10 @@ fn keys_in_vault(vault: &Vault) -> Bitfield {
 fn shortest_path_to_get_all_keys(vault_contents: String) -> u32 {
     let vault = Vault::new(vault_contents);
 
-    let key_distance_maps = key_distance_maps_for_each_key_in_vault(&vault);
+    let key_distance_maps = vec![key_distance_maps_for_each_key_in_vault(&vault)];
     let keys_to_find = keys_in_vault(&vault);
 
-    find_shortest_path(STARTING_KEY, keys_to_find, &key_distance_maps)
+    find_shortest_path(keys_to_find, &key_distance_maps)
 }
 
 pub fn eighteen_a() -> u32 {
@@ -321,32 +327,36 @@ pub fn eighteen_b() -> u32 {
     let topleft: String = contents
         .lines()
         .take(41)
+        // TODO join with newlines
         .map(|line| line.chars().take(41).collect::<String>())
-        .collect();
+        .collect::<Vec<String>>()
+        .join("\n");
     let bottomleft: String = contents
         .lines()
         .skip(40)
         .take(41)
         .map(|line| line.chars().take(41).collect::<String>())
-        .collect();
+        .collect::<Vec<String>>()
+        .join("\n");
     let topright: String = contents
         .lines()
         .take(41)
         .map(|line| line.chars().skip(40).take(41).collect::<String>())
-        .collect();
+        .collect::<Vec<String>>()
+        .join("\n");
     let bottomright: String = contents
         .lines()
         .skip(40)
         .take(41)
         .map(|line| line.chars().skip(40).take(41).collect::<String>())
-        .collect();
+        .collect::<Vec<String>>()
+        .join("\n");
 
-    let vaults: Vec<Vault> = [topleft, bottomleft, topright, bottomright]
+    let distance_maps_per_vault: Vec<_> = [topleft, bottomleft, topright, bottomright]
         .iter()
         .map(|contents| Vault::new(contents.clone()))
+        .map(|vault| key_distance_maps_for_each_key_in_vault(&vault))
         .collect();
-
-    //dbg!(vaults.iter().map(|vault| &vault.keys).collect::<Vec<_>>());
 
     5
 }
