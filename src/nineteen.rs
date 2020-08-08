5b97ea07124073c3c0fd823414575d10db0c29af
jrheard <jrheard@cs.stanford.edu>
2020-08-08 11:29:01 -0700

    WIP

--- src/nineteen.rs
+++ src/nineteen.rs
@@ -1,10 +1,11 @@
 use crate::computer::load_program;
 use crate::computer::{Computer, HaltReason};
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 struct Position(u32, u32);
 
 fn position_is_in_beam(x: u32, y: u32, memory: &[i64]) -> bool {
+    // TODO is there really not a way to reuse computers??
     let mut computer = Computer::new(memory.to_vec());
     computer.push_input(x as i64);
     computer.push_input(y as i64);
@@ -59,35 +60,54 @@ fn step_right_cursor(position: Position, memory: &[i64]) -> Position {
     Position(x - 1, y)
 }
 
-pub fn nineteen_b() -> u32 {
-    let memory = load_program("src/inputs/19.txt");
+fn find_topleft_of_first_bounding_box(box_size: u32, filename: &str) -> Position {
+    let memory = load_program(filename);
 
-    let mut left_cursor = Position(5, 13);
-    let mut right_cursor = Position(6, 13);
+    let mut left_cursor = Position(0, 0);
+    let mut right_cursor = Position(0, 0);
 
-    let mut right_line = vec![0, 0, 0, 0, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5];
+    for y in 1..15 {
+        let mut beam_exists_at_this_y_position = false;
+        let mut farthest_left = 0;
+        let mut farthest_right = 0;
 
-    loop {
-        if left_cursor.1 % 100 == 0 {
-            dbg!(left_cursor, right_cursor);
-        }
+        for x in 0..20 {
+            if position_is_in_beam(x, y, &memory) {
+                beam_exists_at_this_y_position = true;
+                if farthest_left == 0 {
+                    farthest_left = x;
+                }
+                farthest_right = farthest_right.max(x);
+            }
 
-        //println!("stepping left {:?}, right {:?}", left_cursor, right_cursor);
-
-        left_cursor = step_left_cursor(left_cursor, &memory);
-        right_cursor = step_right_cursor(right_cursor, &memory);
-        right_line.push(right_cursor.0);
-
-        if left_cursor.1 > 200 {
-            let right_x_100_steps_up = right_line[left_cursor.1 as usize - 100];
-
-            if right_x_100_steps_up > left_cursor.0 && right_x_100_steps_up - left_cursor.0 >= 100 {
-                break;
+            if beam_exists_at_this_y_position {
+                left_cursor = Position(farthest_left, y);
+                right_cursor = Position(farthest_right, y);
             }
         }
     }
 
-    left_cursor.0 * 10000 + (left_cursor.1 - 100)
+    for _ in 0..box_size {
+        left_cursor = step_left_cursor(left_cursor, &memory);
+    }
+
+    loop {
+        println!("stepping left {:?}, right {:?}", left_cursor, right_cursor,);
+
+        left_cursor = step_left_cursor(left_cursor, &memory);
+        right_cursor = step_right_cursor(right_cursor, &memory);
+
+        if right_cursor.0 > left_cursor.0 && right_cursor.0 - left_cursor.0 >= box_size {
+            break;
+        }
+    }
+
+    Position(left_cursor.0, right_cursor.1)
+}
+
+pub fn nineteen_b() -> u32 {
+    let position = find_topleft_of_first_bounding_box(100, "src/inputs/19.txt");
+    position.0 * 10000 + position.1
 }
 
 #[cfg(test)]
@@ -98,5 +118,13 @@ mod tests {
     fn test_solutions() {
         assert_eq!(nineteen_a(), 166);
         assert_eq!(nineteen_b(), 0);
+    }
+
+    #[test]
+    fn test_sample() {
+        assert_eq!(
+            find_topleft_of_first_bounding_box(10, "src/inputs/19_sample_1.txt"),
+            Position(25, 20)
+        );
     }
 }
