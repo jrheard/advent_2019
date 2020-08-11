9b3831bb59188b2af4e2f03948378cee705dbbb5
jrheard <jrheard@cs.stanford.edu>
2020-08-10 18:58:20 -0700

    WIP on 22a

--- src/twenty_two.rs
+++ src/twenty_two.rs
@@ -38,6 +38,43 @@ fn parse_instructions(filename: &str) -> Vec<Instruction> {
         .collect()
 }
 
+fn shuffle(num_cards: usize, instructions: &[Instruction]) -> Vec<usize> {
+    let mut deck: Vec<usize> = (0..num_cards).collect();
+
+    for instruction in instructions {
+        match instruction {
+            Instruction::DealIntoNewStack => deck.reverse(),
+            Instruction::Cut(offset) => {
+                if *offset > 0 {
+                    let (top, bottom) = deck.split_at(*offset as usize);
+                    deck = [bottom, top].concat();
+                } else {
+                    let (top, bottom) = deck.split_at((deck.len() as i32 + *offset) as usize);
+                    deck = [bottom, top].concat();
+                }
+            }
+            Instruction::DealWithIncrement(step) => {
+                let mut new_deck = vec![0; deck.len()];
+                let mut old_deck_index = 0;
+                let mut new_deck_index = 0;
+                let mut num_dealt = 0;
+
+                while num_dealt < deck.len() {
+                    new_deck[new_deck_index] = deck[old_deck_index];
+                    new_deck_index += step;
+                    new_deck_index %= deck.len();
+                    old_deck_index += 1;
+                    num_dealt += 1;
+                }
+
+                deck = new_deck;
+            }
+        }
+    }
+
+    deck
+}
+
 pub fn twenty_two_a() -> usize {
     5
 }
@@ -74,5 +111,20 @@ mod tests {
                 Instruction::Cut(-2),
             ]
         );
+    }
+
+    #[test]
+    fn test_shuffle() {
+        let instructions = parse_instructions("src/inputs/22_sample_1.txt");
+        let deck = shuffle(10, &instructions);
+        assert_eq!(deck, vec![0, 3, 6, 9, 2, 5, 8, 1, 4, 7,]);
+
+        let instructions = parse_instructions("src/inputs/22_sample_2.txt");
+        let deck = shuffle(10, &instructions);
+        assert_eq!(deck, vec![3, 0, 7, 4, 1, 8, 5, 2, 9, 6]);
+
+        let instructions = parse_instructions("src/inputs/22_sample_3.txt");
+        let deck = shuffle(10, &instructions);
+        assert_eq!(deck, vec![6, 3, 0, 7, 4, 1, 8, 5, 2, 9]);
     }
 }
