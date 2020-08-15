4aba84e0db58f14591ebf261ad561db2a6c04790
jrheard <jrheard@cs.stanford.edu>
2020-08-15 10:33:52 -0700

    cleanup

--- src/twenty_three.rs
+++ src/twenty_three.rs
@@ -20,50 +20,49 @@ pub fn twenty_three_a() -> i64 {
         let mailbox = Arc::clone(&mailbox);
 
         handles.push(thread::spawn(move || {
-            println!("{} spawned", i);
             let mut computer = Computer::new(memory);
             computer.push_input(i);
 
             loop {
-                println!("{} a", i);
                 {
                     let mut mailbox: MutexGuard<Vec<VecDeque<Message>>> = mailbox.lock().unwrap();
 
+                    // Check to see if the program's done.
                     if !mailbox[255].is_empty() {
-                        println!("{} done", i);
-                        // Done!
                         break;
                     }
 
+                    // Check our own mail to see if we have any messages.
                     if let Some(message) = mailbox[i as usize].pop_front() {
-                        println!("{} taking input {:?}", i, message);
                         computer.push_input(message.x);
                         computer.push_input(message.y);
                     }
                 }
 
-                println!("{} b", i);
                 let halt_reason = computer.run(HaltReason::NeedsInput);
                 if halt_reason == HaltReason::Output {
+                    // This computer has produced a message!
+                    // Let's turn it into a Message and stuff it in the mailbox.
                     computer.run(HaltReason::Output);
                     computer.run(HaltReason::Output);
 
                     let mut mailbox = mailbox.lock().unwrap();
-                    println!("{} sending message", i);
                     mailbox[computer.pop_output().unwrap() as usize].push_back(Message {
                         x: computer.pop_output().unwrap(),
                         y: computer.pop_output().unwrap(),
                     });
-                    println!("{} c", i);
                 }
             }
         }));
     }
 
+    // Wait for all the threads to complete.
     for handle in handles {
         let _ = handle.join();
     }
 
+    // "Boot up all 50 computers and attach them to your network.
+    // What is the Y value of the first packet sent to address 255?"
     let relevant_mailbox = &mailbox.lock().unwrap()[255];
     relevant_mailbox[0].y
 }
@@ -74,6 +73,6 @@ mod tests {
 
     #[test]
     fn test_solutions() {
-        assert_eq!(twenty_three_a(), 0);
+        assert_eq!(twenty_three_a(), 23886);
     }
 }
