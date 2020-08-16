49515fc25ead4f79adb2feda89507da494d359dd
jrheard <jrheard@cs.stanford.edu>
2020-08-16 16:12:09 -0700

    WIP

--- src/twenty_four.rs
+++ src/twenty_four.rs
@@ -1,37 +1,6 @@
 use std::collections::{HashMap, HashSet};
 use std::fs;
 
-// TODO lazy_static hashset?
-static OUTER_BORDER_POSITIONS: [(i32, i32); 16] = [
-    (0, 0),
-    (1, 0),
-    (2, 0),
-    (3, 0),
-    (4, 0),
-    (0, 1),
-    (0, 2),
-    (0, 3),
-    (4, 1),
-    (4, 2),
-    (4, 3),
-    (0, 4),
-    (1, 4),
-    (2, 4),
-    (3, 4),
-    (4, 4),
-];
-
-static INNER_BORDER_POSITIONS: [(i32, i32); 8] = [
-    (1, 1),
-    (2, 1),
-    (3, 1),
-    (1, 2),
-    (3, 2),
-    (1, 3),
-    (2, 3),
-    (3, 3),
-];
-
 #[derive(Debug, Copy, Clone, PartialEq)]
 enum Cell {
     Alive,
@@ -42,16 +11,109 @@ enum Cell {
 struct Position {
     x: i32,
     y: i32,
-    depth: i32,
 }
 
 #[derive(Debug)]
 struct Grid {
-    levels: HashMap<i32, Vec<Cell>>,
+    levels: Vec<Level>,
     width: usize,
     height: usize,
     min_depth: i32,
     max_depth: i32,
+}
+
+#[derive(Debug)]
+struct Level {
+    cells: Vec<Cell>,
+    width: usize,
+    height: usize,
+}
+
+impl Level {
+    fn get(&self, position: Position) -> Cell {
+        self.cells[(position.x + self.width as i32 * position.y) as usize]
+    }
+
+    fn num_alive_cells_in_row(&self, y: usize) -> usize {
+        (0..self.width)
+            .into_iter()
+            .map(|x| {
+                self.get(Position {
+                    x: x as i32,
+                    y: y as i32,
+                })
+            })
+            .filter(|cell| *cell == Cell::Alive)
+            .count()
+    }
+
+    fn num_alive_cells_in_column(&self, x: usize) -> usize {
+        (0..self.height)
+            .into_iter()
+            .map(|y| {
+                self.get(Position {
+                    x: x as i32,
+                    y: y as i32,
+                })
+            })
+            .filter(|cell| *cell == Cell::Alive)
+            .count()
+    }
+
+    fn num_alive_neighbors(&self, position: Position, outer: &Level, inner: &Level) -> usize {
+        let mut num_alive = 0;
+
+        let cardinal_direction_neighbors = [
+            Position {
+                x: position.x - 1,
+                y: position.y,
+            },
+            Position {
+                x: position.x + 1,
+                y: position.y,
+            },
+            Position {
+                x: position.x,
+                y: position.y - 1,
+            },
+            Position {
+                x: position.x,
+                y: position.y + 1,
+            },
+        ];
+
+        let count_cell = |cell| match cell {
+            Cell::Alive => 1,
+            Cell::Dead => 0,
+        };
+
+        for neighbor in cardinal_direction_neighbors.iter() {
+            // 1: Handle positions that are off of the grid, i.e. part of the "outer" level.
+            if neighbor.x < 0 {
+                num_alive += count_cell(outer.get(Position { x: 1, y: 2 }));
+            } else if neighbor.x > 4 {
+                num_alive += count_cell(outer.get(Position { x: 3, y: 2 }));
+            } else if neighbor.y < 0 {
+                num_alive += count_cell(outer.get(Position { x: 2, y: 1 }));
+            } else if neighbor.y > 4 {
+                num_alive += count_cell(outer.get(Position { x: 2, y: 3 }));
+            } else if neighbor.x == 2 && neighbor.y == 2 {
+                // 2: Handle the (2, 2) neighbor position, which refers to the "inner" level.
+                num_alive += match (position.x, position.y) {
+                    (1, _) => inner.num_alive_cells_in_column(0),
+                    (3, _) => inner.num_alive_cells_in_column(4),
+                    (_, 1) => inner.num_alive_cells_in_row(0),
+                    (_, 3) => inner.num_alive_cells_in_row(4),
+                    _ => unreachable!(),
+                }
+            } else {
+                // 3: All other positions refer to cells on _this_ level.
+                num_alive += count_cell(self.get(position));
+            }
+        }
+
+        num_alive
+    }
 }
 
 impl Grid {
@@ -66,16 +128,29 @@ impl Grid {
                 match c {
                     '.' => cells.push(Cell::Dead),
                     '#' => cells.push(Cell::Alive),
-                    _ => unimplemented!(),
+                    _ => unreachable!(),
                 }
             }
         }
 
-        let mut levels = HashMap::new();
-        levels.insert(0, cells);
-
         Grid {
-            levels,
+            levels: vec![
+                Level {
+                    cells: vec![Cell::Dead; cells.len()],
+                    width,
+                    height,
+                },
+                Level {
+                    cells,
+                    width,
+                    height,
+                },
+                Level {
+                    cells: vec![Cell::Dead; cells.len()],
+                    width,
+                    height,
+                },
+            ],
             width,
             height,
             min_depth: 0,
@@ -83,115 +158,8 @@ impl Grid {
         }
     }
 
-    fn get(&self, position: Position) -> Cell {
-        let cells = self.levels[&position.depth];
-
-        cells[(position.x + self.width as i32 * position.y) as usize]
-    }
-
     fn position_is_in_bounds(&self, Position { x, y, .. }: Position) -> bool {
         x >= 0 && (x as usize) < self.width && y >= 0 && (y as usize) < self.height
-    }
-
-    fn neighbor_positions(&self, position: Position) -> impl Iterator<Item = Position> {
-        let cardinal_direction_neighbors = [
-            Position {
-                x: position.x - 1,
-                y: position.y,
-                depth: position.depth,
-            },
-            Position {
-                x: position.x + 1,
-                y: position.y,
-                depth: position.depth,
-            },
-            Position {
-                x: position.x,
-                y: position.y - 1,
-                depth: position.depth,
-            },
-            Position {
-                x: position.x,
-                y: position.y + 1,
-                depth: position.depth,
-            },
-        ];
-
-        // TODO first a map that handles one level up
-
-        cardinal_direction_neighbors
-            .iter()
-            .map(|neighbor| {
-                // 1: Handle positions that are off of the grid, i.e. one level "up", i.e. one level negative.
-                // TODO see if this looks better as a match
-                if neighbor.x < 0 {
-                    Position {
-                        x: 1,
-                        y: 2,
-                        depth: neighbor.depth - 1,
-                    }
-                } else if neighbor.x > 4 {
-                    Position {
-                        x: 3,
-                        y: 2,
-                        depth: neighbor.depth - 1,
-                    }
-                } else if neighbor.y < 0 {
-                    Position {
-                        x: 2,
-                        y: 1,
-                        depth: neighbor.depth - 1,
-                    }
-                } else if neighbor.y > 4 {
-                    Position {
-                        x: 2,
-                        y: 3,
-                        depth: neighbor.depth - 1,
-                    }
-                } else {
-                    *neighbor
-                }
-            })
-            .flat_map(|neighbor| {
-                if neighbor.x == 2 && neighbor.y == 2 {
-                    // 2: Handle the center position, i.e. positions that are one level "down", i.e. one level positive
-                    match (position.x, position.y) {
-                        (1, 2) => (0..self.height)
-                            .into_iter()
-                            .map(|y| Position {
-                                x: 0,
-                                y: y as i32,
-                                depth: neighbor.depth + 1,
-                            })
-                            .collect::<Vec<_>>(),
-
-                        (3, 2) => (0..self.height)
-                            .into_iter()
-                            .map(|y| Position {
-                                x: 4,
-                                y: y as i32,
-                                depth: neighbor.depth + 1,
-                            })
-                            .collect::<Vec<_>>(),
-                        _ => unreachable!(),
-                    }
-                } else {
-                    [neighbor].iter()
-                }
-            })
-    }
-
-    fn num_alive_neighbors(&self, position: Position) -> usize {
-        // one or more helper functions that return impl Iterator?
-        //(0..self.width).into_iter().map(|x| Position {
-        //x,
-        //y: 0,
-        //depth: neighbor.depth - 1,
-        //})
-
-        self.neighbor_positions(position)
-            .filter(|&pos| self.get(pos) == Cell::Alive)
-            .count()
     }
 
     fn tick(&self) -> Grid {
